c4493833761b79f790b7570478c38744c3236814
PremAnanth2002 <112572885+PremAnanth2002@users.noreply.github.com>
2023-05-22 22:56:45 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+function Footer() {
+  return (
+    <Container fluid className="footer">
+      <p>prem</p>
+    </Container>
+  );
+}
+
+export default Footer;
